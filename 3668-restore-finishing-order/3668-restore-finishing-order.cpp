9cad0aef7e6d2f8e68bfa600be6c7a4d250e8e85
Prajapati Vineet <vineet.prajapati.cg@gmail.com>
2026-06-09 15:04:03 +0530

    Time: 0 ms (100%), Space: 49.9 MB (98.96%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int>ans;
+        for(int i = 0 ; i<order.size() ; i++){
+            for(int j = 0; j<friends.size() ; j++){
+                if(order[i]==friends[j]){
+                    ans.push_back(order[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
